bc20054da850d10153759c444cfcf33364b8e436
Samuel Hernandes <slhernandes01@google.com>
2023-06-29 19:55:39 +0200

    finished fr fr

--- lua/SLHernandes/remap.lua
+++ lua/SLHernandes/remap.lua
@@ -1,6 +1,10 @@
-vim.keymap.set("n", "<leader>pb", vim.cmd.Ex)
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")

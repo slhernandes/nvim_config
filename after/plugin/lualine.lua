0dfd19881bf69eb4042a812bf471a0522480986d
Samuel Hernandes <samuelhernandes@pop-os.localdomain>
2023-07-24 21:04:49 +0200

    feat: change lualine separator

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -7,7 +7,7 @@ require('lualine').setup {
 --    component_separators = { left = '', right = ''},
 --    section_separators = { left = '', right = ''},
     component_separators = { left = '/', right = '/'},
-    section_separators = { left = '◤', right = '◢'},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},

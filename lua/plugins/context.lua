5a61141204f79f75eb69431dfcc728eb85a8c169
slhernandes <30470091+slhernandes@users.noreply.github.com>
2024-01-03 23:44:47 +0100

    Update context.lua

--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
-  config = require('treesitter-context').setup({
+  config = function ()
+    require('treesitter-context').setup({
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
     min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
@@ -14,4 +15,5 @@ return {
     zindex = 20, -- The Z-index of the context window
     on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
   })
+  end,
 }

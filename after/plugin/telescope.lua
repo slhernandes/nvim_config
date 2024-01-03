0d3578682e579f9b35e998d1c3f98d5dab7e2831
Samuel Hernandes <slhernandes01@gmail.com>
2024-01-03 22:09:09 +0100

    feat: toggleterm plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,9 +5,6 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>pn', function()
-  require('telescope').extensions.notify.notify()
-end)
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 --require("telescope").setup {
 --  defaults = {
--- lua/SLHernandes/packer.lua
+++ lua/SLHernandes/packer.lua
@@ -3,64 +3,56 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup({function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require('packer').startup({
+  function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use("folke/tokyonight.nvim")
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('nvim-treesitter/playground')
-  use('ThePrimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      {                          -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },                -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },            -- Required
-      { 'L3MON4D3/LuaSnip' },                -- Required
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+      -- or                            , branch = '0.1.x',
+      requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use("folke/tokyonight.nvim")
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      requires = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
+        {                          -- Optional
+          'williamboman/mason.nvim',
+          run = function()
+            pcall(vim.cmd, 'MasonUpdate')
+          end,
+        },
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },                -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },            -- Required
+        { 'L3MON4D3/LuaSnip' },                -- Required
+      }
+    }
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use 'nvim-tree/nvim-web-devicons'
+    use 'prichrd/netrw.nvim'
+    use 'nvim-treesitter/nvim-treesitter-context'
+    use 'lervag/vimtex'
+    use { "akinsho/toggleterm.nvim", tag = '*' }
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end
     }
   }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use 'nvim-tree/nvim-web-devicons'
-  use 'prichrd/netrw.nvim'
-  use 'nvim-treesitter/nvim-treesitter-context'
-  use 'lervag/vimtex'
---  use {
---    "folke/noice.nvim",
---    requires = {
---      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
---      "MunifTanjim/nui.nvim",
---      -- OPTIONAL:
---      --   `nvim-notify` is only needed, if you want to use the notification view.
---      --   If not available, we use `mini` as the fallback
---      "rcarriga/nvim-notify",
---    }
---  }
-end,
-config = {
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'single' })
-    end
-  }
-}})
+})

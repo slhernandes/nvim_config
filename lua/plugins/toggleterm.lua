63cca8e72ec309b6f117820fa3cd36075ea3fc20
Samuel Hernandes <slhernandes01@gmail.com>
2024-01-18 00:08:37 +0100

    feat: Tweaked toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -17,11 +17,11 @@ return {
       autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
       start_in_insert = true,
       insert_mappings = false, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+      terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
       persist_size = true,
       persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
       direction = 'vertical',
-      close_on_exit = false, -- close the terminal window when the process exits
+      close_on_exit = true, -- close the terminal window when the process exits
       -- Change the default shell. Can be a string or a function returning a string
       shell = vim.o.shell,
       auto_scroll = true, -- automatically scroll to the bottom on terminal output

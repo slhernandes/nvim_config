2ca10358b56523e82ccf1381f29e7023a0c11f15
Samuel L. Hernandes <slhernandes01@gmail.com>
2025-11-01 03:12:11 +0100

    Disabled pylsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ return {
     'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/nvim-cmp'
   },
   config = function()
-    require("mason").setup({ui = {border = "rounded"}})
+    require("mason").setup({ ui = { border = "rounded" } })
 
     local servers = {
       zls = {},
@@ -13,54 +13,56 @@ return {
       texlab = {},
       gopls = {},
       hyprls = {},
-      qmlls = {cmd = {"qmlls", "-E"}, root_markers = {".qmlls.ini"}},
+      qmlls = { cmd = { "qmlls", "-E" }, root_markers = { ".qmlls.ini" } },
       rust_analyzer = {
         settings = {
-          diagnostics = {disabled = {"unlinked-file"}},
+          diagnostics = { disabled = { "unlinked-file" } },
           root_dir = ".",
-          init_options = {detachedFiles = {"/tmp/file.rs"}}
+          init_options = { detachedFiles = { "/tmp/file.rs" } }
         }
       },
-      pylsp = {
-        settings = {
-          pylsp = {
-            plugins = {
-              jedi_completion = {
-                include_class_objects = true,
-                include_function_objects = true,
-                eager = true
-              }
-            }
-          }
-        }
-      },
+      -- pylsp = {
+      --   settings = {
+      --     pylsp = {
+      --       plugins = {
+      --         jedi_completion = {
+      --           include_class_objects = true,
+      --           include_function_objects = true,
+      --           eager = true
+      --         }
+      --       }
+      --     }
+      --   }
+      -- },
       lua_ls = {
         on_init = function(client)
           if client.workspace_folders then
             local path = client.workspace_folders[1].name
             if vim.uv.fs_stat(path .. '/.luarc.json') or
-                vim.uv.fs_stat(path .. '/.luarc.jsonc') then return end
+                vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+              return
+            end
           end
 
           client.config.settings.Lua = vim.tbl_deep_extend('force',
-                                                           client.config
-                                                               .settings.Lua, {
-            runtime = {version = 'LuaJIT'},
-            workspace = {
-              checkThirdParty = false,
-              library = {vim.env.VIMRUNTIME}
-            }
-          })
+            client.config
+            .settings.Lua, {
+              runtime = { version = 'LuaJIT' },
+              workspace = {
+                checkThirdParty = false,
+                library = { vim.env.VIMRUNTIME }
+              }
+            })
         end,
         settings = {
           Lua = {
-            diagnostics = {globals = {'vim'}, disable = {'missing-fields'}}
+            diagnostics = { globals = { 'vim' }, disable = { 'missing-fields' } }
           }
         }
       }
     }
 
-    for _, method in ipairs({'textDocument/diagnostic', 'workspace/diagnostic'}) do
+    for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
       local default_diagnostic_handler = vim.lsp.handlers[method]
       vim.lsp.handlers[method] = function(err, result, context, config)
         if err ~= nil and err.code == -32802 then return end
@@ -73,29 +75,29 @@ return {
     capabilities_clangd.offsetEncoding = 'utf-32'
 
     local lsp_attach = function(_, bufnr)
-      local opts = {buffer = bufnr, remap = false}
+      local opts = { buffer = bufnr, remap = false }
 
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       vim.keymap.set("n", "K",
-                     function() vim.lsp.buf.hover({border = "rounded"}) end,
-                     opts)
+        function() vim.lsp.buf.hover({ border = "rounded" }) end,
+        opts)
       vim.keymap.set("n", "<leader>vws",
-                     function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count = 1}) end,
-                     opts)
+        function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1 }) end,
+        opts)
       vim.keymap.set("n", "]d",
-                     function() vim.diagnostic.jump({count = -1}) end, opts)
+        function() vim.diagnostic.jump({ count = -1 }) end, opts)
       vim.keymap.set("n", "<leader>vca",
-                     function() vim.lsp.buf.code_action() end, opts)
+        function() vim.lsp.buf.code_action() end, opts)
       vim.keymap.set("n", "<leader>vrr",
-                     function() vim.lsp.buf.references() end, opts)
+        function() vim.lsp.buf.references() end, opts)
       vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end,
-                     opts)
+        opts)
       vim.keymap.set("i", "<C-h>", function()
-        vim.lsp.buf.signature_help({border = "rounded"})
+        vim.lsp.buf.signature_help({ border = "rounded" })
       end, opts)
       vim.keymap.set("n", "<leader>vsh", function()
-        vim.lsp.buf.signature_help({border = "rounded"})
+        vim.lsp.buf.signature_help({ border = "rounded" })
       end, opts)
     end
 
@@ -107,7 +109,7 @@ return {
         server_cfg.capabilities = capabilities
       end
       server_cfg.on_attach = server_cfg.on_attach or lsp_attach
-      server_cfg.root_markers = server_cfg.root_markers or {".git"}
+      server_cfg.root_markers = server_cfg.root_markers or { ".git" }
       vim.lsp.enable(server_name)
       vim.lsp.config(server_name, server_cfg)
     end

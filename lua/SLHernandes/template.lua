5429d47c9b23a6f6a5303ae6fa91ddb5e0110157
Samuel L. Hernandes <slhernandes01@gmail.com>
2025-12-16 04:31:32 +0100

    Added new template for typst file

--- lua/SLHernandes/template.lua
+++ lua/SLHernandes/template.lua
@@ -1,15 +1,18 @@
 vim.api.nvim_exec2([[
   autocmd BufNewFile [0-9]*.cpp 0r ~/.config/nvim/templates/template.cpp
-  ]], {output = false})
+  ]], { output = false })
 vim.api.nvim_exec2([[
   autocmd BufNewFile aoc*.rs 0r ~/.config/nvim/templates/template.rs
-  ]], {output = false})
+  ]], { output = false })
 vim.api.nvim_exec2([[
   autocmd BufNewFile *.cc 0r ~/.config/nvim/templates/opp_template.cc
-  ]], {output = false})
+  ]], { output = false })
 vim.api.nvim_exec2([[
   autocmd BufNewFile *.ned 0r ~/.config/nvim/templates/opp_template.ned
-  ]], {output = false})
+  ]], { output = false })
 vim.api.nvim_exec2([[
   autocmd BufNewFile *.ini 0r ~/.config/nvim/templates/opp_template.ini
-  ]], {output = false})
+  ]], { output = false })
+vim.api.nvim_exec2([[
+  autocmd BufNewFile *.typ 0r ~/.config/nvim/templates/template.typ
+  ]], { output = false })
---
+++ templates/template.typ
@@ -0,0 +1,23 @@
+#import "@preview/fletcher:0.5.7"
+#import fletcher: node, diagram, edge
+#import fletcher.shapes: rect, pill
+#set text(
+  font: "Noto Sans",
+  style: "normal",
+  weight: "regular",
+)
+
+#set page(
+  paper: "a4",
+  header: align(right, text(size: 8pt)[
+    //Samuel Lewi Hernandes 03742862
+  ]),
+  numbering: "1",
+)
+
+#align(center, text(size: 16pt, weight: "bold")[
+  // Machine Learning and Optimization\
+  // Problem Set 7
+])
+
+#set heading(numbering: none)

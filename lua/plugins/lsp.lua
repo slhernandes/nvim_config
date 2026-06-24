6085145767aa3536a88bd1c26f6eb7cadebf25ab
Samuel L. Hernandes <slhernandes01@gmail.com>
2026-06-24 15:43:32 +0200

    Changed guicursor to "a:block" and added fennel-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,22 +3,23 @@ vim.pack.add({
   "https://github.com/mason-org/mason.nvim"
 })
 
-require("mason").setup({ui = {border = "rounded"}})
+require("mason").setup({ ui = { border = "rounded" } })
 
 local servers = {
   ty = {},
   zls = {},
   tinymist = {},
+  fennel_ls = {},
   clangd = {},
   texlab = {},
   gopls = {},
   hyprls = {},
-  qmlls = {cmd = {"qmlls", "-E"}, root_markers = {".qmlls.ini"}},
+  qmlls = { cmd = { "qmlls", "-E" }, root_markers = { ".qmlls.ini" } },
   rust_analyzer = {
     settings = {
-      diagnostics = {disabled = {"unlinked-file"}},
+      diagnostics = { disabled = { "unlinked-file" } },
       root_dir = ".",
-      init_options = {detachedFiles = {"/tmp/file.rs"}}
+      init_options = { detachedFiles = { "/tmp/file.rs" } }
     }
   },
   lua_ls = {
@@ -26,22 +27,24 @@ local servers = {
       if client.workspace_folders then
         local path = client.workspace_folders[1].name
         if vim.uv.fs_stat(path .. '/.luarc.json') or
-            vim.uv.fs_stat(path .. '/.luarc.jsonc') then return end
+            vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
       end
 
       client.config.settings.Lua = vim.tbl_deep_extend('force', client.config
-                                                           .settings.Lua, {
-        runtime = {version = 'LuaJIT'},
-        workspace = {checkThirdParty = false, library = {vim.env.VIMRUNTIME}}
-      })
+        .settings.Lua, {
+          runtime = { version = 'LuaJIT' },
+          workspace = { checkThirdParty = false, library = { vim.env.VIMRUNTIME } }
+        })
     end,
     settings = {
-      Lua = {diagnostics = {globals = {'vim'}, disable = {'missing-fields'}}}
+      Lua = { diagnostics = { globals = { 'vim' }, disable = { 'missing-fields' } } }
     }
   }
 }
 
-for _, method in ipairs({'textDocument/diagnostic', 'workspace/diagnostic'}) do
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
   local default_diagnostic_handler = vim.lsp.handlers[method]
   vim.lsp.handlers[method] = function(err, result, context, config)
     if err ~= nil and err.code == -32802 then return end
@@ -49,23 +52,23 @@ for _, method in ipairs({'textDocument/diagnostic', 'workspace/diagnostic'}) do
   end
 end
 
-vim.diagnostic.config({float = {border = "rounded"}})
+vim.diagnostic.config({ float = { border = "rounded" } })
 
 local lsp_attach = function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K",
-                 function() vim.lsp.buf.hover({border = "rounded"}) end, opts)
+    function() vim.lsp.buf.hover({ border = "rounded" }) end, opts)
   vim.keymap.set('n', '<Leader>k', function() vim.diagnostic.open_float() end,
-                 {desc = 'Show line diagnostics'})
+    { desc = 'Show line diagnostics' })
   vim.keymap.set('n', "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
-                 opts)
+    opts)
   vim.keymap.set("n", "<leader>vsh", function()
-    vim.lsp.buf.signature_help({border = "rounded"})
+    vim.lsp.buf.signature_help({ border = "rounded" })
   end, opts)
   vim.keymap.set("i", "<C-h>", function()
-    vim.lsp.buf.signature_help({border = "rounded"})
+    vim.lsp.buf.signature_help({ border = "rounded" })
   end, opts)
   if client.name == "qmlls" then
     client.server_capabilities.semanticTokensProvider = nil
@@ -74,7 +77,7 @@ end
 
 for server_name, server_cfg in pairs(servers) do
   server_cfg.on_attach = server_cfg.on_attach or lsp_attach
-  server_cfg.root_markers = server_cfg.root_markers or {".git"}
+  server_cfg.root_markers = server_cfg.root_markers or { ".git" }
   vim.lsp.enable(server_name)
   vim.lsp.config(server_name, server_cfg)
 end
